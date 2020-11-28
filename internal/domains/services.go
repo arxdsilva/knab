d0343447405a42ecf7e340af4fe70a0fb4154e98
Arthur Silva <arxdsilva@gmail.com>
2020-11-28 16:45:38 -0300

    add PrimaryPort implementation

--- internal/domains/services.go
+++ internal/domains/services.go
@@ -1,1 +1,14 @@
 package domains
+
+import "context"
+
+type port struct {
+	repo SecondaryPort
+	ctx  context.Context
+}
+
+// NewService receives a Secondary Port of domain
+// and instantiates a Primary Port
+func NewService(repo SecondaryPort) PrimaryPort {
+	return &port{repo, context.Background()}
+}
