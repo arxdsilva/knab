4b5cc52b9f2dec1ed49ba197ce2d7e01365c05c3
Arthur Silva <arxdsilva@gmail.com>
2020-11-28 12:57:49 -0300

    load platform config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"github.com/arxdsilva/knab/internal/middlewares"
+	"github.com/arxdsilva/knab/platform/config"
 	"github.com/prest/cmd"
-	"github.com/prest/config"
 	"github.com/prest/config/router"
+	pms "github.com/prest/middlewares"
 )
 
 func main() {
 	config.Load()
+	pms.GetApp()
 	r := router.Get()
 	middlewares.RouterRegister(r)
 	cmd.Execute()
