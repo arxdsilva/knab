f336433206819164bc77cce3de86eb83155af916
Arthur Silva <arxdsilva@gmail.com>
2020-11-28 16:54:34 -0300

    use repository, domain and adapter

--- internal/middlewares/router.go
+++ internal/middlewares/router.go
@@ -3,14 +3,19 @@ package middlewares
 import (
 	"net/http"
 
+	"github.com/arxdsilva/knab/internal/domains"
 	"github.com/arxdsilva/knab/internal/handlers"
+	"github.com/arxdsilva/knab/internal/repository/account"
 	"github.com/gorilla/mux"
 )
 
 func RouterRegister(r *mux.Router) {
-	r.HandleFunc("/", handlers.HealthCheck).Methods(http.MethodGet)
-	r.HandleFunc("/accounts", handlers.CreateAccount).Methods(http.MethodPost)
-	r.HandleFunc("/accounts/{account_id:[0-9]+}", handlers.GetAccount).Methods(http.MethodGet)
+	rep := account.NewRepository()
+	s := domains.NewService(rep)
+	adapter := handlers.NewHTTPPrimaryAdapter(s)
+	r.HandleFunc("/", adapter.HealthCheck).Methods(http.MethodGet)
+	r.HandleFunc("/accounts", adapter.CreateAccount).Methods(http.MethodPost)
+	r.HandleFunc("/accounts/{account_id:[0-9]+}", adapter.GetAccount).Methods(http.MethodGet)
 	//	r.HandleFunc("/accounts/:account_uuid", nil).Methods(http.MethodGet)
-	r.HandleFunc("/transactions", handlers.CreateTransaction).Methods(http.MethodPost)
+	r.HandleFunc("/transactions", adapter.CreateTransaction).Methods(http.MethodPost)
 }
