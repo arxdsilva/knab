d5775bcce4d8dd9254ad48c0bc7b26fa952ca204
Arthur Silva <arxdsilva@gmail.com>
2020-11-28 12:01:08 -0300

    add RouterRegister

--- internal/middlewares/router.go
+++ internal/middlewares/router.go
@@ -1,1 +1,16 @@
 package middlewares
+
+import (
+	"net/http"
+
+	"github.com/arxdsilva/knab/internal/handlers"
+	"github.com/gorilla/mux"
+)
+
+func RouterRegister(r *mux.Router) {
+	r.HandleFunc("/", handlers.HealthCheck).Methods(http.MethodGet)
+	r.HandleFunc("/accounts", nil).Methods(http.MethodPost)
+	r.HandleFunc("/accounts/{account_id:[0-9]+}", nil).Methods(http.MethodGet)
+	//	r.HandleFunc("/accounts/:account_uuid", nil).Methods(http.MethodGet)
+	r.HandleFunc("/transactions", nil).Methods("POST")
+}
