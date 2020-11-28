b91fe0a004bc85ae823c7b092d8a8b75f7d9e3a2
Arthur Silva <arxdsilva@gmail.com>
2020-11-28 12:01:46 -0300

    register router and run API

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/arxdsilva/knab/internal/middlewares"
+	"github.com/prest/cmd"
+	"github.com/prest/config"
+	"github.com/prest/config/router"
+)
 
+func main() {
+	config.Load()
+	r := router.Get()
+	middlewares.RouterRegister(r)
+	cmd.Execute()
 }
