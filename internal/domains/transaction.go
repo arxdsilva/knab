14dd0121088ec019f435a2ceb6dbc3ebd525b9a3
Arthur Silva <arxdsilva@gmail.com>
2020-11-29 19:15:04 -0300

    add transaction primary domain

--- internal/domains/transaction.go
+++ internal/domains/transaction.go
@@ -1,1 +1,12 @@
 package domains
+
+import "github.com/nuveo/dbtime"
+
+type Transaction struct {
+	ID              string      `json:"id"`
+	UUID            string      `json:"uuid"`
+	AccountID       string      `json:"account_id"`
+	OperationTypeID string      `json:"operation_type_id"`
+	Amount          float64     `json:"amount"`
+	EventDate       dbtime.Time `json:"event_date"`
+}
