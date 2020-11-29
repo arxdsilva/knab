d37e009422c0440c04d7d2b39974a119f5b5063a
Arthur Silva <arxdsilva@gmail.com>
2020-11-29 12:42:29 -0300

    finish createAccount handler

--- internal/handlers/accounts.go
+++ internal/handlers/accounts.go
@@ -14,12 +14,24 @@ func (a *HTTPPrimaryAdapter) GetAccount(w http.ResponseWriter, r *http.Request) 
 }
 
 func (a *HTTPPrimaryAdapter) CreateAccount(w http.ResponseWriter, r *http.Request) {
-	c := &domains.Account{}
-	if err := json.NewDecoder(r.Body).Decode(c); err != nil {
+	acc := &domains.Account{}
+	if err := json.NewDecoder(r.Body).Decode(acc); err != nil {
 		glg.Error("[CreateAccount]", "(Decode)", err.Error())
 		errAPI := errors.New("Could not parse request body")
 		http.Error(w, errAPI.Error(), http.StatusBadRequest)
 		return
 	}
-
+	if err = acc.Verify(); err != nil {
+		glg.Error("[CreateAccount]", "(Verify)", err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err = a.service.CreateAccount(acc); err != nil {
+		glg.Error("[CreateAccount]", "(service.CreateAccount)", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	glg.Info("[CreateAccount] success ", acc.UUID)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(acc)
 }
