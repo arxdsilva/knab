95036de3622b6855caaa40cb42e359ddb51796a7
Arthur Silva <arxdsilva@gmail.com>
2020-11-29 12:40:51 -0300

    add primary account struct

--- internal/domains/account.go
+++ internal/domains/account.go
@@ -1,1 +1,25 @@
 package domains
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+// Account is the struct that represents primary port for /account
+type Account struct {
+	DocumentNumber string `json:"document_number"`
+	ID             int64  `json:"id"`
+	UUID           string `json:"uuid"`
+}
+
+// Verify checks if Account's inputs are valid
+func (a *Account) Verify() (err error) {
+	if a.DocumentNumber == "" {
+		return errors.New("Document number cannot be empty")
+	}
+	if _, err := strconv.Atoi(v); err == nil {
+		return fmt.Errorf("%v is not a document number", a.DocumentNumber)
+	}
+	return
+}
