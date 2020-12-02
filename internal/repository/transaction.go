3a1b56c1c8275e86a129a4b01283f812054e3481
Arthur Silva <arxdsilva@gmail.com>
2020-12-02 14:43:42 -0300

    add transaction repository

--- internal/repository/transaction.go
+++ internal/repository/transaction.go
@@ -1,1 +1,14 @@
 package repository
+
+import "github.com/arxdsilva/knab/internal/domains"
+
+type Transaction struct{}
+
+// NewTransaction  retorna um tipo que implementa a porta secundaria
+func NewTransaction() domains.TransactionService {
+	return &Transaction{}
+}
+
+func (t *Transaction) CreateTransaction(dt *domains.Transaction) (err error) {
+	return
+}
