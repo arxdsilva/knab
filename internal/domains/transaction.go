4e98c94a74abd22f18a8bd14e8f1ff2c781f8414
Arthur Silva <arxdsilva@gmail.com>
2020-12-02 14:45:41 -0300

    use AccountID in transaction as integer

--- internal/domains/transaction.go
+++ internal/domains/transaction.go
@@ -9,15 +9,15 @@ import (
 type Transaction struct {
 	ID              string      `json:"id"`
 	UUID            string      `json:"uuid"`
-	AccountID       string      `json:"account_id"`
+	AccountID       int64       `json:"account_id"`
 	OperationTypeID int         `json:"operation_type_id"`
 	Amount          float64     `json:"amount"`
 	EventDate       dbtime.Time `json:"event_date"`
 }
 
 func (t *Transaction) Verify() (err error) {
-	if t.AccountID == "" {
-		return errors.New("account_id cannot be empty")
+	if t.AccountID <= 0 {
+		return errors.New("account_id cannot be zero or negative")
 	}
 	if !IsOperation(t.OperationTypeID) {
 		return errors.New("operation_type_id is invalid")
